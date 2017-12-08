324c05574b36e831ce9447d12b9b01c68bf7bd95
ppark3 <ppark3@stuy.edu>
2017-12-08 14:42:46 -0500

    started dominate.js

---
+++ dominate.js
@@ -0,0 +1,8 @@
+var button = document.getElementByID("b");
+var list = document.getelementByID("thelist");
+var element = <li>item</li>
+button.addEventListener('click', addToList);
+
+var addToList = function(){
+    list.appendChild
+};
